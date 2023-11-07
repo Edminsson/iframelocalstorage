05b482c018331da80d9ef1556374878cefd3902b
Axel Aguilar <axel.aguilar@gmail.se>
2023-11-07 20:38:35 +0100

    adding event handler for load event

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const write = document.getElementById('write');
-const read = document.getElementById('read');
-
-write.addEventListener('click', () => {
-  console.log('writing to local storage');
-  localStorage.setItem('nyckel', 'hejsan!');
-});
-
-read.addEventListener('click', () => {
-  console.log('reading from local storage');
-  const varde = localStorage.getItem('nyckel');
-  const visa = document.getElementById('visa');
-  if (visa) {
-    visa.innerHTML = visa;
-  }
+window.addEventListener("load", () => {
+    const write = document.getElementById('write');
+    const read = document.getElementById('read');
+    
+    write.addEventListener('click', () => {
+      console.log('writing to local storage');
+      localStorage.setItem('nyckel', 'hejsan!');
+    });
+    
+    read.addEventListener('click', () => {
+      console.log('reading from local storage');
+      const varde = localStorage.getItem('nyckel');
+      const visa = document.getElementById('visa');
+      if (visa) {
+        visa.innerHTML = visa;
+      }
+    });    
 });
